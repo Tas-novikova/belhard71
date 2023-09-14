384b61f66f9fe39c1905f500c60ce85b22159d1b
Tas-novikova <tasnovikova@gmail.com>
2023-09-14 09:38:19 +0300

    add .idea

--- lesson3_1.py
+++ lesson3_1.py
@@ -1,6 +1,9 @@
 # TODO: пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 
+
+#  text = 'Nice to meet you'
 text = input()
 split_text = text.split(' ')
-print(f"{'-'.join(split_text)}")
+join_text = '-'.join(split_text)
 print(text.replace(' ', '-'))
+print(f"{join_text}")
---
+++ lesson3_3.py
@@ -0,0 +1,15 @@
+# TODO: Пользователь вводит Имя, Возраст и Город, сформировать приветственное сообщение путем форматирования 3-мя способами
+
+
+# name = 'Alex'
+# age = 25
+# city = 'Minsk'
+name = input()
+age = int(input())
+city = input()
+welcome_message1 = 'Hello! My name is %s, I\'m %d years old and I\'m from %s' % (name, age, city)
+welcome_message2 = 'Hello! My name is {}, I\'m {} years old and I\'m from {}'.format(name, age, city)
+welcome_message3 = f'Hello! My name is {name}, I\'m {age} years old and I\'m from {city}'
+print(welcome_message1)
+print(welcome_message2)
+print(welcome_message3)
