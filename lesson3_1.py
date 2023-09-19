abaefe3329d0cbd8b2d1858d1f76954bae97ef73
Tas-novikova <tasnovikova@gmail.com>
2023-09-19 23:06:41 +0300

    a few corrections

--- lesson3_1.py
+++ lesson3_1.py
@@ -1,8 +1,10 @@
-# TODO: пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
+# TODO пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 
-#  text = 'Nice to meet you'
+# text = 'Nice to meet you'
 text = input()
 split_text = text.split(' ')
 join_text = '-'.join(split_text)
-print(text.replace(' ', '-'))
-print(f"{join_text}")
+change_text1 = text.replace(' ', '-')
+change_text2 = f"{join_text}"
+print(change_text1)
+print(change_text2)
--- lesson3_4.py
+++ lesson3_4.py
@@ -1,9 +1,11 @@
-# TODO: Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
+# TODO Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
 
-
-num1 = input()
-num2 = input()
-num3 = input()
-sum_positive_numbers = bool(num1.isdigit()) + bool(num2.isdigit()) + bool(num3.isdigit())
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+check_num1 = num1 > 0
+check_num2 = num2 > 0
+check_num3 = num3 > 0
+sum_positive_numbers = bool(check_num1) + bool(check_num2) + bool(check_num3)
 sum_negative_numbers = 3 - sum_positive_numbers
 print('Положительных чисел -', sum_positive_numbers, ', отрицательных чисел -', sum_negative_numbers)
