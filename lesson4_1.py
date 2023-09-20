3b770d3712eea2737e2d195337538a3ed011238d
Tas-novikova <tasnovikova@gmail.com>
2023-09-20 15:38:33 +0300

    lesson4 task1

---
+++ lesson4_1.py
@@ -0,0 +1,6 @@
+# TODO Заполнить список степенями числа 2 (от 2^1 до 2^n)
+
+# n = 9
+n = int(input())
+two_in_n = [pow(2, n) for n in range(1, n+1)]  # or  two_in_n = [2 ** n for n in range(1, n+1)]
+print(two_in_n)
