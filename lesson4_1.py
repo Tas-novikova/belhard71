ef8eed4995f05a7e6b926058bf6bcedfcb492878
Tas-novikova <tasnovikova@gmail.com>
2023-09-21 11:46:38 +0300

    change iterator

--- lesson4_1.py
+++ lesson4_1.py
@@ -2,5 +2,5 @@
 
 # n = 9
 n = int(input())
-two_in_n = [pow(2, n) for n in range(1, n+1)]  # or  two_in_n = [2 ** n for n in range(1, n+1)]
+two_in_n = [pow(2, n) for i in range(1, n+1)]  # or  two_in_n = [2 ** n for n in range(1, n+1)]
 print(two_in_n)
