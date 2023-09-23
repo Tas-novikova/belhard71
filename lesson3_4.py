43641593860465d16c74838dc4759f7b7750a522
Tas-novikova <tasnovikova@gmail.com>
2023-09-23 14:50:51 +0300

    del bool() and add comment

--- lesson3_4.py
+++ lesson3_4.py
@@ -7,6 +7,6 @@ num3 = float(input())
 check_num1 = num1 > 0
 check_num2 = num2 > 0
 check_num3 = num3 > 0
-sum_positive_numbers = bool(check_num1) + bool(check_num2) + bool(check_num3)
+sum_positive_numbers = check_num1 + check_num2 + check_num3  # использовать сам класс bool() - плохая практика
 sum_negative_numbers = 3 - sum_positive_numbers
 print('Положительных чисел -', sum_positive_numbers, ', отрицательных чисел -', sum_negative_numbers)
