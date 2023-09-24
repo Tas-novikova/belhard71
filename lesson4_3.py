a7a6b0d3006b0bb44219ba513db570d54462ea6e
Tas-novikova <tasnovikova@gmail.com>
2023-09-24 20:21:32 +0300

    del comments

--- lesson4_3.py
+++ lesson4_3.py
@@ -1,18 +1,6 @@
 # TODO Заполнить словарь где ключами будут выступать числа от 0 до n, а
 # значениями вложенный словарь с ключами "name" и "email", а значения для этих ключей будут браться с клавиатуры
 
-# n = 2
-# name = 'Ann'
-# email = 'ann@gmail.com'
-
-# n = int(input())
-# name = input('Your name')
-# email = input('Your email')
-
-# data = {n: {} for n in range(0, n)}
-# data_name_email = {'name': name, 'email': email}
-# new_data = {n: data_name_email for n in range(0, n)}
-
 n = int(input())
 
 new_data = {n: {'name': input('Your name'), 'email': input('Your email')} for n in range(0, n)}
